b3fd70f9b6a7a80566be64e58fb8812a25ac08df
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-17 00:58:23 +0100

    Added DirtiesContext so that a new context is created and does not depend on previous tests.

--- src/test/java/one/stayfocused/spring_boot_dotenv/DotenvAutoConfigurationTest.java
+++ src/test/java/one/stayfocused/spring_boot_dotenv/DotenvAutoConfigurationTest.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class DotenvAutoConfigurationTest extends DotenvTestBase{
 
     @Autowired
