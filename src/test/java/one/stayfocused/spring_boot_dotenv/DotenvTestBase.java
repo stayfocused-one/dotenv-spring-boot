da48f35865b5c21d65710a0ba39aa977e5a450b9
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 22:14:24 +0100

    Updated reload endpoint to /dotenvReload to follow convention. Updated methods from DotenvTestBase.

--- src/test/java/one/stayfocused/spring_boot_dotenv/DotenvTestBase.java
+++ src/test/java/one/stayfocused/spring_boot_dotenv/DotenvTestBase.java
@@ -1,6 +1,8 @@
 package one.stayfocused.spring_boot_dotenv;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.IOException;
@@ -12,13 +14,13 @@ abstract class DotenvTestBase {
     protected static final Path DOTENV_PATH = Path.of(".env");
     protected static final String DEFAULT_TEST_PROPERTY = "TEST_ENV_VAR=BeforeReload";
 
-    @BeforeEach
-    void createDotenv() throws IOException {
+    @BeforeAll
+    static void createDotenv() throws IOException {
         Files.writeString(DOTENV_PATH, DEFAULT_TEST_PROPERTY);
     }
 
-    @AfterEach
-    void deleteDotenv() throws IOException {
+    @AfterAll
+    static void deleteDotenv() throws IOException {
         Files.deleteIfExists(DOTENV_PATH);
     }
 }
