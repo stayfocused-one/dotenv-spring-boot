1ee84024806c5c228e3285f68792d8d3f67285b2
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 10:36:32 +0100

    Implemented the logic for getting Bean DotenvPropertySource.

--- src/main/java/one/stayfocused/spring_boot_dotenv/DotenvAutoConfiguration.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvAutoConfiguration.java
@@ -1,12 +1,21 @@
 package one.stayfocused.spring_boot_dotenv;
 
 import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+import java.util.Map;
 
 @Slf4j
 @Configuration
 public class DotenvAutoConfiguration {
 
+    @Bean
+    public DotenvPropertySource dotenvPropertySource(Environment environment) {
+        log.info("Initializing DotenvPropertySource...");
+        Map<String, String> dotenvVariables = DotenvPropertySourceLoader.loadDotenv(environment);
+        log.info("DotenvPropertySource initialized. Loaded {} variables.", dotenvVariables.size());
+        return new DotenvPropertySource("dotenv", dotenvVariables);
+    }
 }
