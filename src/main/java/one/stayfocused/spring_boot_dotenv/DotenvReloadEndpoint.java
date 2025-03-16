ab9d8e660947472925988991a34297c34439b0bc
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 14:34:46 +0100

    Implemented DotenvReloadEndpoint to enable .env refresh in runtime.

--- src/main/java/one/stayfocused/spring_boot_dotenv/DotenvReloadEndpoint.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvReloadEndpoint.java
@@ -1,7 +1,22 @@
 package one.stayfocused.spring_boot_dotenv;
 
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
 
-@RestController
+@Component
+@RequiredArgsConstructor
+@Endpoint(id = "dotenv-reload")
 public class DotenvReloadEndpoint {
+
+    private final DotenvReloadService dotenvReloadService;
+
+    @ReadOperation
+    public String reloadDotenv() {
+        boolean reloaded = dotenvReloadService.reload();
+        return reloaded
+                ? "Dotenv successfully reloaded!"
+                : "Dotenv reload is disabled in configuration.";
+    }
 }
