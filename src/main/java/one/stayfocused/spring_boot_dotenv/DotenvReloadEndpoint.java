183032d7b7b12ab23410d790d92568af40aca177
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 22:14:24 +0100

    Updated reload endpoint to /dotenvReload to follow convention. Updated methods from DotenvTestBase.

--- src/main/java/one/stayfocused/spring_boot_dotenv/DotenvReloadEndpoint.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvReloadEndpoint.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-@Endpoint(id = "dotenv-reload")
+@Endpoint(id = "dotenvReload")
 public class DotenvReloadEndpoint {
 
     private final DotenvReloadService dotenvReloadService;
