268922d0e2328c70936d404c5ced95451f2add07
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 10:50:12 +0100

    Updated DotenvAutoConfiguration. Added a check if dotenv enabled in configurations (default true) and logic if false. Added more logs in case variables are not found.

--- src/main/java/one/stayfocused/spring_boot_dotenv/DotenvAutoConfiguration.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvAutoConfiguration.java
@@ -1,5 +1,6 @@
 package one.stayfocused.spring_boot_dotenv;
 
+import jakarta.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +13,24 @@ import java.util.Map;
 public class DotenvAutoConfiguration {
 
     @Bean
-    public DotenvPropertySource dotenvPropertySource(Environment environment) {
+    public DotenvPropertySource dotenvPropertySource(@Nonnull Environment environment) {
+        boolean dotenvEnabled = Boolean.parseBoolean(environment.getProperty("dotenv.enabled", "true"));
+
+        if (!dotenvEnabled) {
+            log.info("Dotenv support disabled via 'dotenv.enabled=false'. Skipping .env loading.");
+            return new DotenvPropertySource("dotenv", Map.of());
+        }
+
         log.info("Initializing DotenvPropertySource...");
         Map<String, String> dotenvVariables = DotenvPropertySourceLoader.loadDotenv(environment);
-        log.info("DotenvPropertySource initialized. Loaded {} variables.", dotenvVariables.size());
+
+        if (dotenvVariables.isEmpty()) {
+            log.warn("DotenvPropertySource initialized, but no environment variables were loaded from .env.");
+        } else {
+            log.info("DotenvPropertySource initialized. Loaded {} variables.", dotenvVariables.size());
+            log.debug("Loaded variables: {}", dotenvVariables.keySet());
+        }
+
         return new DotenvPropertySource("dotenv", dotenvVariables);
     }
 }
