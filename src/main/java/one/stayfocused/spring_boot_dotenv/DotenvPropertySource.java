b541d0d3ce2b4ae9eae2d2441701df95d7714bdd
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 10:31:55 +0100

    Implemented DotenvPropertySource. Implemented the getProperty method so that the received key cannot be null, and the return value can be null because the key may not exist.

--- src/main/java/one/stayfocused/spring_boot_dotenv/DotenvPropertySource.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvPropertySource.java
@@ -1,9 +1,20 @@
 package one.stayfocused.spring_boot_dotenv;
 
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import org.springframework.core.env.PropertySource;
 
 import java.util.Map;
 
 public class DotenvPropertySource extends PropertySource<Map<String, String>> {
 
+    public DotenvPropertySource(String name, Map<String, String> source) {
+        super(name, source);
+    }
+
+    @Override
+    @Nullable
+    public Object getProperty(@Nonnull String name) {
+        return source.get(name);
+    }
 }
