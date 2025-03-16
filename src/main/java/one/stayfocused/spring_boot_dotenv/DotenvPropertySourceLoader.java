126b8a6d34c818f2a13c1e111fc4e70541767ffe
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 10:15:53 +0100

    Implemented method loadDotenv to load variables from .env file.

--- src/main/java/one/stayfocused/spring_boot_dotenv/DotenvPropertySourceLoader.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvPropertySourceLoader.java
@@ -1,8 +1,40 @@
 package one.stayfocused.spring_boot_dotenv;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 public class DotenvPropertySourceLoader {
 
+    public static Map<String, String> loadDotenv(Environment environment) {
+        String dotenvPath = environment.getProperty("dotenv.path", ".env");
+        Map<String, String> dotenvVariables = new HashMap<>();
+
+        try {
+            log.info("Loading .env file from: {}", dotenvPath);
+            List<String> lines =  Files.readAllLines(Paths.get(dotenvPath));
+
+            for (String line : lines) {
+                if (!line.isBlank() && !line.startsWith("#")) {
+                    String[] parts = line.split("=", 2);
+                    if (parts.length == 2) {
+                        dotenvVariables.put(parts[0].trim(), parts[1].trim());
+                        log.debug("Loaded variable: {}=***", parts[0].trim());
+                    }
+                }
+            }
+            log.info(".env file successfully loaded ({} variables)", dotenvVariables.size());
+        } catch (IOException e) {
+            log.warn("Failed to load .env file: {}", dotenvPath, e);
+        }
+
+        return dotenvVariables;
+    }
 }
