accd99919033eb04c8fe291ff473fd61790a3282
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-16 14:33:23 +0100

    Added annotation Inherited.

--- src/main/java/one/stayfocused/spring_boot_dotenv/EnableDotenv.java
+++ src/main/java/one/stayfocused/spring_boot_dotenv/EnableDotenv.java
@@ -7,6 +7,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Documented
+@Inherited
 @Import(DotenvAutoConfiguration.class)
 public @interface EnableDotenv {
 }
