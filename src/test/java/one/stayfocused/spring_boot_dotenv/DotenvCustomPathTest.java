c9e40e70c7865d09a24fe5f95bd397b7bca4c294
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-17 11:02:39 +0100

    Test commit to fix DotenvCustomPathTest.

--- src/test/java/one/stayfocused/spring_boot_dotenv/DotenvCustomPathTest.java
+++ src/test/java/one/stayfocused/spring_boot_dotenv/DotenvCustomPathTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.env.Environment;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -13,6 +14,7 @@ import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(properties = "dotenv.path=src/test/resources/.env")
 class DotenvCustomPathTest {
 
