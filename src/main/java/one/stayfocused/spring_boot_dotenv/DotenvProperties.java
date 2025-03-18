f95c7b75c3f10726185308e2e55099860c0f3124
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-18 23:58:03 +0100

    Created DotenvProperties as configuration property class to generate metadata about properties.

---
+++ src/main/java/one/stayfocused/spring_boot_dotenv/DotenvProperties.java
@@ -0,0 +1,37 @@
+package one.stayfocused.spring_boot_dotenv;
+
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Provides metadata for dotenv settings in application.properties / application.yml.
+ */
+@Getter
+@ConfigurationProperties(prefix = "dotenv")
+public class DotenvProperties {
+
+    /**
+     * Enable or disable loading of the .env file.
+     */
+    private boolean enabled = true;
+
+    /**
+     * The path to the .env file.
+     */
+    private String path = ".env";
+
+    /**
+     * Define whether dotenv variables should have high or low priority.
+     */
+    private String priority = "low";
+
+    /**
+     * Whether the application should fail if the .env file is missing.
+     */
+    private boolean failOnMissing = false;
+
+    /**
+     * Enable or disable the ability to reload the .env file at runtime.
+     */
+    private boolean reloadEnabled = false;
+}
