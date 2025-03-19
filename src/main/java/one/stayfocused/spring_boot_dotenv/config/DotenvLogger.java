f7b67e51e6868583d22ac35703d8db8c364cda3b
Augustin Gerasym <augustingerasym@gmail.com>
2025-03-19 23:30:12 +0100

    Implemented Dotenv logger to enable logs after context initialization.

---
+++ src/main/java/one/stayfocused/spring_boot_dotenv/config/DotenvLogger.java
@@ -0,0 +1,48 @@
+package one.stayfocused.spring_boot_dotenv.config;
+
+import lombok.extern.slf4j.Slf4j;
+import one.stayfocused.spring_boot_dotenv.environment.DotenvPropertySource;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static one.stayfocused.spring_boot_dotenv.core.DotenvUtils.*;
+
+@Slf4j
+@Component
+public class DotenvLogger {
+
+    private final ConfigurableEnvironment environment;
+
+    public DotenvLogger(ConfigurableEnvironment environment) {
+        this.environment = environment;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void logDotenvInfo() {
+        boolean enabled = environment.getPropertySources().contains(PROPERTY_SOURCE_NAME);
+        String dotenvPath = environment.getProperty(DOTENV_PATH_KEY, DEFAULT_ENV_PATH);
+        String priority = environment.getProperty(DOTENV_PRIORITY_KEY, DEFAULT_DOTENV_PRIORITY);
+        String failOnMissing = environment.getProperty(DOTENV_FAIL_ON_MISSING_KEY, String.valueOf(DEFAULT_FAIL_ON_MISSING));
+        boolean reload = Boolean.parseBoolean(environment.getProperty(DOTENV_RELOAD_ENABLED_KEY, String.valueOf(DEFAULT_RELOAD_ENABLED)));
+
+        int variableCount = Optional.ofNullable(environment.getPropertySources().get(PROPERTY_SOURCE_NAME))
+                .filter(DotenvPropertySource.class::isInstance)
+                .map(DotenvPropertySource.class::cast)
+                .map(DotenvPropertySource::getSource)
+                .map(Map::size)
+                .orElse(0);
+
+        log.info("[Dotenv] Status: {}", enabled ? "ENABLED" : "DISABLED");
+        log.info("[Dotenv] Path: {}", dotenvPath);
+        log.info("[Dotenv] Fail on missing: {}", failOnMissing);
+        log.info("[Dotenv] Priority: {}", priority);
+        log.info("[Dotenv] Reload in runtime: {}", reload ? "ENABLED" : "DISABLED");
+        log.info("[Dotenv] Loaded variables: {} ", variableCount);
+    }
+}
